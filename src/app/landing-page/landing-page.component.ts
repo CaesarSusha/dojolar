6222fbc4be65365f0a69d01aa535d487d27f93e1
CaesarSusha <kaiser_alexandra@protonmail.com>
2024-01-20 13:20:47 +0100

    added routing for weather component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MatButton],
+  imports: [MatButton, RouterModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
